19adcf629a20fface0a39fc5a0f435d7f195257f
bauticutraro <bautistacutraro@gmail.com>
2018-01-21 20:27:55 -0300

    end of veloz.html

--- js/script.js
+++ js/script.js
@@ -2,9 +2,5 @@ const nav = document.querySelector('#header');
 
 window.addEventListener('scroll', () => {
   let y = window.pageYOffset;
-  if (y > 150) {
-    nav.classList.add('js-header-bg');
-  }
-  else if (y === 0)
-    nav.classList.remove('js-header-bg');
+  y > 200 ? nav.classList.add('js-header-bg') : nav.classList.remove('js-header-bg');
 });
